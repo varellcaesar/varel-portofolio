0047f1d771dfe696c279270b53fe32b8d2c8f8e2
varel <varellcaesar@gmail.com>
2025-12-22 20:11:51 +0700

    remove package-lock

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -2,9 +2,8 @@ import { Resend } from "resend";
 import { NextResponse } from "next/server";
 import { contactSchema } from "@varel-web/lib/contact-schema";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req: Request) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   try {
     const body = await req.json();
 
